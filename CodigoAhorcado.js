2a7ed240bbbb6d59a23158e221f84f72e3a8dc12
Sergio Moya <info.sergiomoya@gmail.com>
2023-11-14 12:24:55 +0100

    Update CodigoAhorcado.js

---
+++ CodigoAhorcado.js
@@ -0,0 +1,7 @@
+ // Lista de palabras para adivinar
+        const words = ["javascript", "programacion", "desarrollo", "ahorcado", "tecnologia"];
+		const word = words[Math.floor(Math.random()*words.length)];
+		
+  document.querySelector("#calculate").addEventListener("click", () =>{
+  alert	
+})
